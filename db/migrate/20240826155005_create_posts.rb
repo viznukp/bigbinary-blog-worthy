e1fec893201cd042e15ec41a8fd1d5baed732e85
vishnu <vishnukp.me@gmail.com>
2024-08-26 21:53:59 +0530

    Added migration script for creating posts table

---
+++ db/migrate/20240826155005_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :upvotes, default: 0, null: false
+      t.integer :downvotes, default: 0, null: false
+      t.boolean :is_blog_worthy, default: false
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :posts
+  end
+end
