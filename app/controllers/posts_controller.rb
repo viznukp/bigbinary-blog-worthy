bed5b912f60d509a5beb6a81a3d27552af6bc3b1
vishnu <vishnukp.me@gmail.com>
2024-09-07 04:54:53 +0530

    Added validations to post model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,5 @@
+class PostsController < ApplicationController
+  def index
+    render html:"This is from PostsController "
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,19 @@
+class Post < ApplicationRecord
+  MAX_TITLE_LENGTH = 125
+
+  validates :title,
+    presence: true,
+    length: { maximum: MAX_TITLE_LENGTH }
+  validate :non_negative_net_votes
+  validates_inclusion_of :is_blog_worthy, in: [true, false]
+
+  private
+
+    def non_negative_net_votes
+      net_votes = upvotes - downvotes
+
+      if net_votes.negative?
+        errors.add(:base, "Net votes (upvotes - downvotes) cannot be negative")
+      end
+    end
+end
