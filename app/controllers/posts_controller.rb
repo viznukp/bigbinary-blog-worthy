d698b4ec64e6e3b7c7f5be681beb0bec684fcddd
vishnu <vishnukp.me@gmail.com>
2024-09-07 05:26:03 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
   def index
-    render html:"This is from PostsController "
+    render html: "This is from PostsController "
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   MAX_TITLE_LENGTH = 125
 
