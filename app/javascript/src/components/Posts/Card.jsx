c91b283290c5522005273611ac372b2fd0ef9dc3
vishnu <vishnukp.me@gmail.com>
2024-09-09 18:48:11 +0530

    Added neetoui to the project

--- app/javascript/src/components/Posts/Card.jsx
+++ app/javascript/src/components/Posts/Card.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import { Typography } from "@bigbinary/neetoui";
+
 const Card = ({ title, description }) => (
   <div className="w-full rounded-md border bg-blue-50 px-4 py-4">
-    <h2 className="border-b font-semibold">{title}</h2>
-    <h3 className="mt-4 truncate">{description}</h3>
+    <Typography className="border-b" style="h3" weight="medium">
+      {title}
+    </Typography>
+    <Typography className="mt-4 truncate" style="h4" weight="normal">
+      {description}
+    </Typography>
   </div>
 );
 
--- app/javascript/src/components/commons/PageTitle.jsx
+++ app/javascript/src/components/commons/PageTitle.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-import Button from "./Button";
+import { Button } from "@bigbinary/neetoui";
 
 const PageTitle = ({ title }) => (
   <div className="mt-8 flex w-full items-center justify-between">
     <h2 className="text-3xl font-semibold ">{title}</h2>
-    <Button buttonText="Create new post" />
+    <Button
+      className="bg-indigo-600 hover:bg-indigo-700"
+      label="Create new post"
+    />
   </div>
 );
 
